1c19162d559f6f4184cb7c1a736495b5e941d471
jansayd <jansayd@gmail.com>
2022-05-24 05:38:36 -0400

    terceiro commit

---
+++ aula 16 - Arrays (Matrizes).php
@@ -0,0 +1,51 @@
+<?php
+//ARRAYS
+
+//unicos valores
+
+$carros = array ("Fusca","Uno","Gol");
+$carro = ["Fusca", "Uno", "Gol", "Polo"];
+$modelo = ["Fusca", "Uno", "Gol", "Polo","Monza"];
+$idade = array("Jan"=>"30","Maria"=>"31","Lucas"=>"93", );
+$qtd = count ($modelo);
+
+echo "A quantidade na MAtriz e: ".count($carros);
+echo "<br>";
+echo "A quantidade na MAtriz e: ".count($carro);
+echo "<br>";
+
+for ($i=0; $i < $qtd; $i++){
+    echo $modelo[$i];
+    echo "<br>";
+}
+echo "<br>";
+echo "<br>";
+
+foreach ($carros as $tipo){
+    echo $tipo."<br>";
+}
+
+echo "<br>";
+echo "<br>";
+
+// pode-se usar tambem valores chaves
+
+echo $idade ["Lucas"];
+
+echo "<br>";
+echo "<br>";
+
+// assossiativo
+
+foreach ($idade as $chave => $valor){
+    echo "O nome é $chave é a idade é $valor <br>";
+}
+
+echo "<br>";
+echo "<br>";
+
+
+
+?>
+
+
